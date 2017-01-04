9517597160e17e2b749b274d2d5657610ae7d7fa
avatar <szpetyus@gmail.com>
2017-01-04 08:28:51 +0100

    könyvtár nevek normalizálása file nevek normalizálása számlaszám megállapítás stabilizálása

--- szamle/DownloadHandler.cs
+++ szamle/DownloadHandler.cs
@@ -38,20 +38,20 @@ namespace szamle
             if (!callback.IsDisposed)
             {
                 String thisPath = _defaultPath;
-                IEnumerable<Invoice> thisInvoice = MainWindow.invoiceIpc.invoices.Where(x => downloadItem.SuggestedFileName.Contains(x.norm_szamlaszam));
+                IEnumerable<Invoice> thisInvoice = MainWindow.invoiceIpc.invoices.Where(x => !String.IsNullOrWhiteSpace(x.norm_szamlaszam) && downloadItem.SuggestedFileName.Contains(x.norm_szamlaszam));
                 if (thisInvoice != null && thisInvoice.Count()>0 && thisInvoice.First() != null)
                 {
-                    thisPath = _defaultPath.Replace(MainWindow.mask_ev, thisInvoice.First().kibocsatas.Substring(0, 4)).Replace(MainWindow.mask_szolgaltato, thisInvoice.First().szolgaltato);
+                    thisPath = legalisePath( _defaultPath.Replace(MainWindow.mask_ev, thisInvoice.First().kibocsatas.Substring(0, 4)).Replace(MainWindow.mask_szolgaltato, thisInvoice.First().szolgaltato));
                     System.IO.Directory.CreateDirectory(thisPath);
                 }
                 else
                 {
-                    thisPath = _defaultPath.Replace(MainWindow.mask_ev, "").Replace(MainWindow.mask_szolgaltato, "").Replace(@"\\", @"\"); ;
+                    thisPath = legalisePath( _defaultPath.Replace(MainWindow.mask_ev, "").Replace(MainWindow.mask_szolgaltato, "").Replace(@"\\", @"\"));
                     System.IO.Directory.CreateDirectory(thisPath);
                 }
                 using (callback)
                 {
-                    callback.Continue(System.IO.Path.Combine(thisPath, downloadItem.SuggestedFileName), showDialog: false);
+                    callback.Continue(System.IO.Path.Combine(thisPath, legaliseFileName( downloadItem.SuggestedFileName)), showDialog: false);
                 }
             }
         }
@@ -68,6 +68,20 @@ namespace szamle
                 CefSharp.LoadingStateChangedEventArgs e = new LoadingStateChangedEventArgs(browser, true, false, false);
                 OnLoadingStateChanged(e);
             }
+        }
+
+        private String legalisePath(String path)
+        {
+            string regexSearch = new string(System.IO.Path.GetInvalidPathChars());
+            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(string.Format("[{0}]", System.Text.RegularExpressions.Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
+        }
+
+        private String legaliseFileName(String filename)
+        {
+            string regexSearch = new string(System.IO.Path.GetInvalidPathChars());
+            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(string.Format("[{0}]", System.Text.RegularExpressions.Regex.Escape(regexSearch)));
+            return r.Replace(filename, "");
         }
     }
 }
